7e4f57ac818fdedcb35a8fcbf017e82618cd0338
Miles Crane <miles.crane@stu.barren.kyschools.us>
2023-01-20 11:15:28 -0600

    finished exercise but not bonus

---
+++ script.js
@@ -0,0 +1,23 @@
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+console.log(warmHugs);
+
+console.log(warmHugs.toUpperCase());
+
+warmHugs = warmHugs.replace("like", "love");
+console.log(warmHugs);
+
+let beenImpaled = "Oh, look at that. I've been impaled.";
+console.log(beenImpaled);
+
+console.log(beenImpaled.slice(18, 36));
+
+const dotDotDot = "...";
+console.log(dotDotDot);
+
+let skullBones = `I don't have a skull${dotDotDot}or bones.`
+console.log(skullBones);
+
+console.log(Math.PI);
+
+let randomNumber = Math.floor(Math.random() * 3) + 1;
+console.log(randomNumber); 
